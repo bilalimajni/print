0e2216ec74a921ac7e9726937c6c6d3b7b43a66b
Bilal Imajni <bilal.imajni@ens.uvsq.fr>
2023-03-02 21:50:53 +0100

    creation : event listener , point

---
+++ assets/script.js
@@ -0,0 +1,71 @@
+const slides = [
+	{
+		"image": "./assets/images/slideshow/slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>" 
+	},
+	{
+		"image": "./assets/images/slideshow/slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+	
+		"image": "./assets/images/slideshow/slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image": "./assets/images/slideshow/slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+	},
+	
+];
+
+// importer les claases
+
+const suivant = document.querySelector(".arrow_right");
+const precedent = document.querySelector(".arrow_left");
+
+
+// Mettre un event listener sur chacune des flèches
+ suivant.addEventListener("click", function () { });
+
+precedent.addEventListener("click", function () { });
+
+// creation des points
+const contaner =  document.querySelector(".dots" ) ;
+
+
+
+function creatpoint () {
+
+	const newP = document.createElement("p");
+	newP.classList.add("dot");
+	contaner.appendChild(newP);
+};
+
+
+function creatDot_selected () {
+
+	const newP = document.createElement("p");
+	newP.classList.add("dot","dot_selected");
+	contaner.appendChild(newP);
+};
+
+  
+
+
+
+
+  
+// creation de point selon le nombre d’éléments présents dans le tableau “
+
+
+for (var i = 0; i < slides.length; i++) {
+	
+	creatpoint ();
+  };
+
+
+
+  // changment dimage 
+
+
